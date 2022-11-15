616bdd3ed8258e0c5d85421938b6fda2ef28ee2f
edgar <edgar.solf@mailbox.tu-dresden.de>
2022-11-15 09:48:00 +0100

    Added Maven Structure

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        SimpleLinkedList<Integer> test = new SimpleLinkedList<>();
+        test.add(123);
+        test.add(234);
+        System.out.println(test.size());
+    }
+}
