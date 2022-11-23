0bd0358788830fa1306343934cedcefda3f16536
hagendaehn1 <hagendaehne25@outlook.de>
2022-11-23 18:11:39 +0100

    removed bugs

--- src/main/java/SimpleLinkedList.java
+++ src/main/java/SimpleLinkedList.java
@@ -30,7 +30,7 @@ public class SimpleLinkedList<E> extends AbstractCollection<E> implements Collec
         if (start == null) {
             start = e;
         }
-        if (end != null); {
+        if (end != null) {
             end.next = e;
         }
         end = e;
@@ -57,10 +57,9 @@ public class SimpleLinkedList<E> extends AbstractCollection<E> implements Collec
 
         @Override
         public E next() {
-            //Elem temp = current;
+            Elem temp = current;
             current = current.next;
-            return current.elem;
-            //return temp.elem;
+            return temp.elem;
         }
 
         @Override
